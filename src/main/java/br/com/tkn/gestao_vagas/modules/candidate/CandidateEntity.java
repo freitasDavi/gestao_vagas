199c196c43f70f1e934bda13d4d6f61e45c5c5df
Davi PC <davi.frrs@gmail.com>
2023-12-28 14:24:16 -0300

    first commit

---
+++ src/main/java/br/com/tkn/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,29 @@
+package br.com.tkn.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class CandidateEntity {
+    
+    private UUID id;
+    private String name;
+
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
+    private String username;
+
+    @Email(message = "O campo [email] deve ser um email válido")
+    private String email;
+
+    @Length(min = 10, max = 100, message = "O campo [password] deve ter entre 10 e 100 caracteres")
+    private String password;
+    private String description;
+    private String curriculum;
+
+
+}
